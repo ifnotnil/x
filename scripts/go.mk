d99c503506732e655bcd502a4b5f54fae2b7e2c2
Kostas Stamatakis <moukoublen@gmail.com>
2025-05-06 22:51:46 +0300

    init

---
+++ scripts/go.mk
@@ -0,0 +1,43 @@
+MODULE := $(shell cat go.mod | grep -e "^module" | sed "s/^module //")
+
+GO_PACKAGES = go list -tags='$(TAGS)' ./...
+GO_FOLDERS = go list -tags='$(TAGS)' -f '{{ .Dir }}' ./...
+GO_FILES = find . -type f -name '*.go'
+
+export GO111MODULE := on
+
+.PHONY: mod
+mod:
+	go mod tidy
+	go mod verify
+
+# https://go.dev/ref/mod#go-get
+# -u flag tells go get to upgrade modules
+# -t flag tells go get to consider modules needed to build tests of packages named on the command line.
+# When -t and -u are used together, go get will update test dependencies as well.
+.PHONY: go-deps-upgrade
+go-deps-upgrade:
+	go get -u -t ./...
+	go mod tidy
+
+# https://pkg.go.dev/cmd/go#hdr-Compile_packages_and_dependencies
+# https://pkg.go.dev/cmd/compile
+# https://pkg.go.dev/cmd/link
+
+.PHONY: test
+test:
+	CGO_ENABLED=1 go test -timeout 60s -race -tags='$(TAGS)' -coverprofile=coverage.txt -covermode=atomic ./...
+
+.PHONY: test-n-read
+test-n-read: test
+	@go tool cover -func coverage.txt
+
+.PHONY: bench
+bench: # runs all benchmarks
+	CGO_ENABLED=1 go test -benchmem -run=^Benchmark$$ -mod=readonly -count=1 -v -race -bench=. ./...
+
+.PHONY: caches-info
+caches-info:
+	@echo "cache folders: "
+	@du -h -d 0 -a "$$(go env GOMODCACHE)"
+	@du -h -d 0 -a "$$(go env GOCACHE)"
---
+++ scripts/lib.mk
@@ -0,0 +1,62 @@
+# require variable REPO_ROOT
+
+## https://www.gnu.org/software/make/manual/html_node/Parallel-Disable.html
+.NOTPARALLEL:
+
+# man git-clean
+.PHONY: git-reset
+git-reset:
+	git reset --hard
+	git clean -fd
+
+.PHONY: env
+env:
+	@echo -e "\e[0;90m>>>\e[0m \e[0;94m Module \e[0m \e[0;90m<<<\e[0m"
+	@echo "$(MODULE)"
+	@echo ""
+
+	@echo -e "\e[0;90m>>>\e[0m \e[0;94m Go env \e[0m \e[0;90m<<<\e[0m"
+	go env
+	@echo ""
+
+	@echo -e "\e[0;90m>>>\e[0m \e[0;94m Packages \e[0m \e[0;90m<<<\e[0m"
+	$(GO_PACKAGES)
+	@echo ""
+
+	@echo -e "\e[0;90m>>>\e[0m \e[0;94m Folders \e[0m \e[0;90m<<<\e[0m"
+	$(GO_FOLDERS)
+	@echo ""
+
+	@echo -e "\e[0;90m>>>\e[0m \e[0;94m Files \e[0m \e[0;90m<<<\e[0m"
+	$(GO_FILES)
+	@echo ""
+
+	@echo -e "\e[0;90m>>>\e[0m \e[0;94m Tools \e[0m \e[0;90m<<<\e[0m"
+	@echo '$(TOOLS_BIN)'
+	@echo ""
+
+	@echo -e "\e[0;90m>>>\e[0m \e[0;94m Path \e[0m \e[0;90m<<<\e[0m"
+	@echo "$${PATH}" | tr ':' '\n'
+	@echo ""
+
+	@echo -e "\e[0;90m>>>\e[0m \e[0;94m Shell \e[0m \e[0;90m<<<\e[0m"
+	@echo "SHELL         :$${SHELL}"
+	@echo "BASH          :$${BASH}"
+	@echo "BASH_VERSION  :$${BASH_VERSION}"
+	@echo "BASH_VERSINFO :$${BASH_VERSINFO}"
+	@echo ""
+
+.PHONY: checks
+checks: vet staticcheck gofumpt goimports golangci-lint
+
+.PHONY: ci-format
+ci-format: goimports gofumpt
+	$(REPO_ROOT)/scripts/git-check-dirty
+
+.PHONY: ci-mod
+ci-mod: mod
+	$(REPO_ROOT)/scripts/git-check-dirty
+
+.PHONY: ci-sh
+ci-sh: shfmt shellcheck
+	@$(REPO_ROOT)/scripts/git-check-dirty
